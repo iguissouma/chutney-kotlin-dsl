6abf8de8f4da7ee9911349f1c4b16eae5723235d
iguissouma <issam.guissouma@laposte.net>
2020-09-09 23:35:12 +0200

    chore: add test for scenario with sub steps

--- src/test/kotlin/com/chutneytesting/kotlin/dsl/ChutneyScenarioDslTest.kt
+++ src/test/kotlin/com/chutneytesting/kotlin/dsl/ChutneyScenarioDslTest.kt
@@ -66,4 +66,80 @@ class ChutneyScenarioDslTest {
 
     }
 
+    @Test
+    fun `able to create chutney scenario with substeps using kotlin dsl`() {
+
+        val scenario = Scenario(title = "SWAPI GET people record") {
+            Given("I set get people service api endpoint") {
+                Step("set id") {
+                    ContextPutTask(entries = mapOf("id" to "1"))
+                }
+                Step("set uri") {
+                    ContextPutTask(entries = mapOf("uri" to "api/people/${"id".spEL()}"))
+                }
+            }
+            When("I send GET HTTP request") {
+                HttpGetTask(target = "swapi.dev", uri = "uri".spEL())
+            }
+            Then("I receive valid HTTP response") {
+                JsonAssertTask(document = "body".spEL(), expected = mapOf("$.name" to "Luke Skywalker"))
+            }
+        }
+
+        "$scenario" `should equal json` """
+            {
+              "title" : "SWAPI GET people record",
+              "description" : "SWAPI GET people record",
+              "givens" : [ {
+                "description" : "I set get people service api endpoint",
+                "subSteps" : [ {
+                  "description" : "set id",
+                  "implementation" : {
+                    "type" : "context-put",
+                    "inputs" : {
+                      "entries" : {
+                        "id" : "1"
+                      }
+                    }
+                  }
+                }, {
+                  "description" : "set uri",
+                  "implementation" : {
+                    "type" : "context-put",
+                    "inputs" : {
+                      "entries" : {
+                        "uri" : "api/people/${'$'}{#id}"
+                      }
+                    }
+                  }
+                } ]
+              } ],
+              "when" : {
+                "description" : "I send GET HTTP request",
+                "implementation" : {
+                  "type" : "http-get",
+                  "target" : "swapi.dev",
+                  "inputs" : {
+                    "uri" : "${'$'}{#uri}",
+                    "timeout" : "2 sec"
+                  }
+                }
+              },
+              "thens" : [ {
+                "description" : "I receive valid HTTP response",
+                "implementation" : {
+                  "type" : "json-assert",
+                  "inputs" : {
+                    "document" : "${'$'}{#body}",
+                    "expected" : {
+                      "${'$'}.name" : "Luke Skywalker"
+                    }
+                  }
+                }
+              } ]
+            }
+        """.trimIndent()
+
+    }
+
 }
