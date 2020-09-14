f75b06b5806db7f6ab854c1696b06371fe163907
GENSOLLEN Matthieu <matthieu-externe.gensollen@erdf.fr>
2020-09-14 10:21:46 +0200

    chore(): Add build tests logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.0"
 }
+
 group = "me.iguissouma"
 version = "1.0-SNAPSHOT"
 
@@ -13,12 +14,21 @@ repositories {
         setUrl("https://dl.bintray.com/s1m0nw1/KtsRunner")
     }
 }
+
 dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.5")
     testImplementation(kotlin("test-junit"))
     testImplementation("com.gregwoodfill.assert:kotlin-json-assert:0.1.0")
     testImplementation("de.swirtz:ktsRunner:0.0.8")
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.test {
+  testLogging {
+    events("passed", "failed", "skipped")
+    showStandardStreams = true
+  }
 }
